667cbafffd08af053632f813fe993181676b82f5
wangyuan <799086345@qq.com>
2016-05-27 20:11:19 +0800

    修改素数模板

--- cpp/prime.cpp
+++ cpp/prime.cpp
@@ -12,6 +12,8 @@ struct _mod<i,2>{
 	static const int value = ( i % 2 );
 };
 
+// sushu 1
+
 template<int i>
 struct sushu{
 	static const int num = i;
@@ -36,6 +38,32 @@ void coutsushu<2>(){
 	printf("2 ");
 }
 
+// sushu 2
+template<int i>
+struct sushu2: public sushu2<i-1>{
+	typedef sushu2<i-1> base;
+
+	const static int num = i;
+	static const int value = _mod<i,i-1>::value;
+
+	sushu2(){
+		if(sushu2<i>::value)
+			printf("%d ",sushu2<i>::num );
+	};
+};
+
+template<>
+struct sushu2<2>{
+	static const int num = 2;
+	static const int value = 1;
+	sushu2(){
+		printf("2 ");
+	};
+};
+
+
+
 int main(){
-	coutsushu<100>();
+	// coutsushu<1000>();
+	sushu2<100> _;
 };
